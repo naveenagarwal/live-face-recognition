21c02f1509f13a59758d93bd6d4c97234e085977
Naveen Agarwal <naveenagarwal287@gmail.com>
2018-04-16 09:45:39 +0530

    accuracy fix

--- live_face_recognition.py
+++ live_face_recognition.py
@@ -76,27 +76,29 @@ def show_face_at_location(img, location):
 
 def compare_and_show(unknown_image):
     temp_encoding = fg.face_encodings(unknown_image) 
-
+    
     if len(temp_encoding) < 1:
         return True
-
+    temp_encoding = [temp_encoding[0]]
     for entry_titlize in training_set:
+        print("Checking for profile {}".format(entry_titlize))
         count = 0
         
         for unknown_encoding in temp_encoding:
             result = fg.compare_faces(
                 training_set[entry_titlize]["encodings"], unknown_encoding)
-        
+            print(result)
+            result = [result[0]]
             if True in result and not training_set[entry_titlize]["image_displayed"]:
                 
-                show_face_at_location(unknown_image, count)
-                training_set[entry_titlize]["image_displayed"] = True
+                # show_face_at_location(unknown_image, count)
+                # training_set[entry_titlize]["image_displayed"] = True
 
                 if not training_set[entry_titlize]["greeted"]:
                     greetings = greeting_list[random.randint(0,total_greetings)]
-                    os.system(
-                        "say 'Welcome {}, {}'".format(entry_titlize, greetings))
-                    training_set[entry_titlize]["greeted"] = True
+                    # os.system(
+                    #     "say 'Welcome {}, {}'".format(entry_titlize, greetings))
+                    # training_set[entry_titlize]["greeted"] = True
 
             count = count + 1
 
@@ -125,7 +127,7 @@ def lets_go():
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
             
-            time.sleep(5)
+            time.sleep(1)
         finally:
             print("releasing video handler")
             video_capture.release()
